00d1f3c6fb917927650fc1cf7110210e1735b6af
Derek DeRoo <derek.l.deroo@gmail.com>
2022-11-26 17:46:08 -0600

    commit tree 70d955a7456e46d2262745377345efc41fa1d0cb

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+if (someNumber < 400) {
+  return 'This one is on me!'
+} else if(someNumber < 2000) {
+  return 'That will be twenty bucks.'
+} else if(someNumber < 2500) {
+  return 'I will gladly take your thirty bucks.'
+} else {
+  return 'No can do.'
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+if (someCity === 'NYC') {
+  return 'Ok, sounds good.'
+} else {
+  return 'No go.'
+}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(someAmount){
+if (someAmount === 'generous') {
+  return 'Thank you so much.'
+} else if (someAmount === 'not as generous'){
+  return 'Thank you.'
+} else {
+  return 'Bye.'
+}
 }
